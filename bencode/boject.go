596e8d5ac5a26fb738df4dd1221b605669b41e54
Akimio521 <akimio2333@gmail.com>
2025-03-16 14:10:50 +0800

    feat(bencode): 实现 BObject 的 Bencode 编码功能

--- bencode/boject.go
+++ bencode/boject.go
@@ -1,5 +1,10 @@
 package bencode
 
+import (
+	"bufio"
+	"io"
+)
+
 type BObject struct {
 	t BType // 类型
 	v any   // 值
@@ -36,6 +41,72 @@ func getBType[T allowedTypes]() BType {
 	default:
 		panic("unsupported type")
 	}
+}
+
+// 将 BObject 编码写入 Writer 中，返回写入的字节长度
+func (o *BObject) Bencode(w io.Writer) (int, error) {
+	bw := bufio.NewWriter(w)
+
+	wLen := 0
+	switch o.t {
+	case BSTR:
+		str, err := Get[string](o)
+		if err != nil {
+			return 0, err
+		}
+		n, err := EncodeString(bw, str)
+		if err != nil {
+			return 0, err
+		}
+		wLen += n
+	case BINT:
+		str, err := Get[int](o)
+		if err != nil {
+			return 0, err
+		}
+		n, err := EncodeInt(bw, str)
+		if err != nil {
+			return 0, err
+		}
+		wLen += n
+	case BLIST:
+		bw.WriteByte('l')
+		list, err := Get[[]*BObject](o)
+		if err != nil {
+			return 0, err
+		}
+		for _, elem := range list {
+			n, err := elem.Bencode(bw)
+			if err != nil {
+				return 0, nil
+			}
+			wLen += n
+		}
+		bw.WriteByte('e')
+		wLen += 2 // 一个是"l"一个是"e"
+	case BDICT:
+		bw.WriteByte('d')
+		dict, err := Get[map[string]*BObject](o)
+		if err != nil {
+			return 0, nil
+		}
+		for k, v := range dict {
+			n, err := EncodeString(bw, k)
+			if err != nil {
+				return 0, nil
+			}
+			wLen += n
+			n, err = v.Bencode(bw)
+			if err != nil {
+				return 0, nil
+			}
+			wLen += n
+		}
+		bw.WriteByte('e')
+		wLen += 2 // 一个是"d"一个是"e"
+	}
+	bw.Flush()
+	return wLen, nil
 }
 
 func GetBOject[T allowedTypes](v T) *BObject {
