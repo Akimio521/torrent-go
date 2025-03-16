f547fbda1401f4c30549b2a02a9f6acc69c16de8
Akimio521 <akimio2333@gmail.com>
2025-03-16 15:14:09 +0800

    test(bencode): 添加 BObject 解析功能的测试用例，包括基本类型和复合类型

--- bencode/bencode_test.go
+++ bencode/bencode_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"math"
+	"strings"
 	"testing"
 
 	"github.com/Akimio521/torrent-go/bencode"
@@ -269,6 +270,229 @@ func TestNestedStructures(t *testing.T) {
 			_, err := tc.obj.Bencode(buf)
 			require.NoError(t, err)
 			require.Equal(t, tc.expected, buf.String())
+		})
+	}
+}
+
+func TestParse(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    string
+		expected func() *bencode.BObject
+		wantErr  error
+	}{
+		// -------------------- 基础类型测试 --------------------
+		{
+			name:  "BSTR basic",
+			input: "3:foo",
+			expected: func() *bencode.BObject {
+				return bencode.GetBOject("foo")
+			},
+		},
+		{
+			name:  "BINT positive",
+			input: "i42e",
+			expected: func() *bencode.BObject {
+				return bencode.GetBOject(42)
+			},
+		},
+		{
+			name:  "BINT negative",
+			input: "i-123e",
+			expected: func() *bencode.BObject {
+				return bencode.GetBOject(-123)
+			},
+		},
+		{
+			name:  "BINT zero",
+			input: "i0e",
+			expected: func() *bencode.BObject {
+				return bencode.GetBOject(0)
+			},
+		},
+
+		// -------------------- 复合类型测试 --------------------
+		{
+			name:  "BLIST empty",
+			input: "le",
+			expected: func() *bencode.BObject {
+				return bencode.GetBOject([]*bencode.BObject{})
+			},
+		},
+		{
+			name:  "BLIST nested",
+			input: "li42el3:abcee",
+			expected: func() *bencode.BObject {
+				return bencode.GetBOject([]*bencode.BObject{
+					bencode.GetBOject(42),
+					bencode.GetBOject([]*bencode.BObject{
+						bencode.GetBOject("abc"),
+					}),
+				})
+			},
+		},
+		{
+			name:  "BDICT ordered keys",
+			input: "d1:ai1e1:zi2ee",
+			expected: func() *bencode.BObject {
+				return bencode.GetBOject(map[string]*bencode.BObject{
+					"a": bencode.GetBOject(1),
+					"z": bencode.GetBOject(2),
+				})
+			},
+		},
+		{
+			name:  "BDICT nested",
+			input: "d4:infod6:lengthi1024e5:filesld4:pathl3:dir8:file.txteeeee",
+			expected: func() *bencode.BObject {
+				return bencode.GetBOject(map[string]*bencode.BObject{
+					"info": bencode.GetBOject(map[string]*bencode.BObject{
+						"length": bencode.GetBOject(1024),
+						"files": bencode.GetBOject([]*bencode.BObject{
+							bencode.GetBOject(map[string]*bencode.BObject{
+								"path": bencode.GetBOject([]*bencode.BObject{
+									bencode.GetBOject("dir"),
+									bencode.GetBOject("file.txt"),
+								}),
+							}),
+						}),
+					}),
+				})
+			},
+		},
+
+		// -------------------- 错误场景测试 --------------------
+		{
+			name:    "Invalid prefix",
+			input:   "x123",
+			wantErr: bencode.ErrInvalidBObject,
+		},
+		{
+			name:    "Truncated integer",
+			input:   "i123",
+			wantErr: io.ErrUnexpectedEOF,
+		},
+		{
+			name:    "Invalid string length",
+			input:   "5:abc",
+			wantErr: io.ErrUnexpectedEOF,
+		},
+		{
+			name:  "Unordered dict keys",
+			input: "d1:zi2e1:ai1ee",
+			expected: func() *bencode.BObject {
+				return bencode.GetBOject(map[string]*bencode.BObject{
+					"a": bencode.GetBOject(1),
+					"z": bencode.GetBOject(2),
+				})
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// 执行解析
+			r := bytes.NewReader([]byte(tc.input))
+			obj, err := bencode.Parse(r)
+
+			// 错误处理
+			if tc.wantErr != nil {
+				require.ErrorIs(t, err, tc.wantErr)
+				return
+			}
+			require.NoError(t, err)
+
+			// 验证结果
+			expectedObj := tc.expected()
+			validateBObject(t, expectedObj, obj)
+		})
+	}
+}
+
+// ---------------------- 验证工具函数 ----------------------
+func validateBObject(t *testing.T, expected, actual *bencode.BObject) {
+	require.Equal(t, expected.GetBType(), actual.GetBType())
+
+	switch expected.GetBType() {
+	case bencode.BSTR:
+		ev, _ := bencode.Get[string](expected)
+		av, _ := bencode.Get[string](actual)
+		require.Equal(t, ev, av)
+
+	case bencode.BINT:
+		ev, _ := bencode.Get[int](expected)
+		av, _ := bencode.Get[int](actual)
+		require.Equal(t, ev, av)
+
+	case bencode.BLIST:
+		elist, _ := bencode.Get[[]*bencode.BObject](expected)
+		alist, _ := bencode.Get[[]*bencode.BObject](actual)
+		require.Equal(t, len(elist), len(alist))
+		for i := range elist {
+			validateBObject(t, elist[i], alist[i])
+		}
+
+	case bencode.BDICT:
+		edict, _ := bencode.Get[map[string]*bencode.BObject](expected)
+		adict, _ := bencode.Get[map[string]*bencode.BObject](actual)
+		require.Equal(t, len(edict), len(adict))
+		for k, ev := range edict {
+			av, exists := adict[k]
+			require.True(t, exists)
+			validateBObject(t, ev, av)
+		}
+	}
+}
+
+// ---------------------- 边界条件测试 ----------------------
+func TestEdgeCases(t *testing.T) {
+	t.Run("Empty input", func(t *testing.T) {
+		_, err := bencode.Parse(bytes.NewReader(nil))
+		require.ErrorIs(t, err, io.EOF)
+	})
+
+	t.Run("Huge integer", func(t *testing.T) {
+		input := "i9223372036854775807e" // math.MaxInt64
+		obj, err := bencode.Parse(strings.NewReader(input))
+		require.NoError(t, err)
+		val, err := bencode.Get[int](obj)
+		require.NoError(t, err)
+		require.Equal(t, 9223372036854775807, val)
+	})
+
+	t.Run("Nested depth limit", func(t *testing.T) {
+		deepNested := strings.Repeat("l", 1000) + strings.Repeat("e", 1000)
+		_, err := bencode.Parse(strings.NewReader(deepNested))
+		require.NoError(t, err) // 验证无栈溢出
+	})
+}
+
+// ---------------------- 性能测试 ----------------------
+// goos: darwin
+// goarch: arm64
+// pkg: github.com/Akimio521/torrent-go/bencode
+// cpu: Apple M1
+// BenchmarkParse
+// BenchmarkParse/simple
+// BenchmarkParse/simple-8         	 2541420	       493.4 ns/op	    4248 B/op	       4 allocs/op
+// BenchmarkParse/string
+// BenchmarkParse/string-8         	 2355640	       502.6 ns/op	    4296 B/op	       7 allocs/op
+// BenchmarkParse/complex
+// BenchmarkParse/complex-8        	  975999	      1227 ns/op	    5376 B/op	      37 allocs/op
+// PASS
+func BenchmarkParse(b *testing.B) {
+	testData := map[string]string{
+		"simple":  "i42e",
+		"string":  "11:hello_world",
+		"complex": "d4:infod6:lengthi1024e5:filesld4:pathl3:dir8:file.txteeeee",
+	}
+
+	for name, data := range testData {
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				r := strings.NewReader(data)
+				_, _ = bencode.Parse(r)
+			}
 		})
 	}
 }
